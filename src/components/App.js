4da6cc672dac6f78755732f213a3582f334921a9
sanyam59 <100775998+sanyam59@users.noreply.github.com>
2022-10-02 15:46:56 +0530

    Update App.js
    Answer

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,30 @@ import React, { useState } from 'react';
 
 export default function App() {
  //code here 
-
+ consr App=()=>{
+  const[input,setInput]=useState(''};
+const[term,setTerm]=useState(''};
+ const handleChange=(e)=>{
+   setInput(e.target.value);
+  }
+  const handleSubmit=(e)=>{
+   e.preventDefault();
+   setTerm(term+input);
+   setInput('');
+  }
+  
  
   return (
     <div>
-  <input id='input' value={inputValue} onChange={changeInput} />
+  <input id='' type="text" value={input} onChange={handleChange} />
   
       <p id='intro'>Concated String</p>
       <p id='text'>{text} </p>
-      <button id='button' onClick={buttonClick}>
-        Click me
+      <button id='' onClick={handleSubmit}>
+        Submit
       </button>
+ <p>{term}</p>
     </div>
   );
 }
+ export defauly App;
