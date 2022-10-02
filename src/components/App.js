1e6f9751677e17b5a515d1ae8f9e48d7f0fe3ac8
sanyam59 <100775998+sanyam59@users.noreply.github.com>
2022-10-02 16:15:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,27 @@
-
 import '../styles/App.css';
 import React, { useState } from 'react';
 
 export default function App() {
- //code here 
- consr App=()=>{
-  const[input,setInput]=useState("};
-const[term,setTerm]=useState("};
- const handleChange=(e)=>{
-   setInput(e.target.value);
-  }
-  const handleSubmit=(e)=>{
-   e.preventDefault();
-   setTerm(term+input);
-   setInput('');
-  }
+  const [inputValue , setInputvalue] = useState('');
+  const [text , setText] = useState('')
+
+  const changeInput =(e)=>{
+    setInputvalue(e.target.value)
   
- 
+  }
+  const buttonClick =()=>{
+    setText((prev)=> prev + inputValue);
+    setInputvalue('')
+  }
   return (
     <div>
-  <input id='' type="text" value={input} onChange={handleChange} />
-  
-      <p id='intro'>Concated String</p>
-      <p id='text'>{text} </p>
-      <button id='' onClick={handleSubmit}>
-        Submit
-      </button>
- <p>{term}</p>
-    </div>
+    <input id='input' value={inputValue} onChange={(e)=>changeInput(e)} />
+    
+        <p id='intro'>Concated String</p>
+        <p id='text'>{text}</p>
+        <button id='button' onClick={()=>buttonClick()}>
+          Click me
+        </button>
+      </div>
   );
 }
- export defauly App;
